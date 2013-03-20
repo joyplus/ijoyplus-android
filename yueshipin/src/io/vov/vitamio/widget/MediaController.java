acdbc3a6452dd97863f8c8a261db9e195c929052
YuMinghe <veteranyu@163.com>
2013-03-20 15:42:36 +0800

    yu

--- yueshipin/src/io/vov/vitamio/widget/MediaController.java
+++ yueshipin/src/io/vov/vitamio/widget/MediaController.java
@@ -539,7 +539,6 @@ public class MediaController extends FrameLayout  {
 //			mFileName.setText(mTitle);
 	}
 	public void DisableButtom(){
-		if(mTextViewDownloadRate!=null)
 		mTextViewDownloadRate.setVisibility(View.INVISIBLE);
 		mimageView33.setVisibility(View.INVISIBLE);
 		mQualityButton.setVisibility(View.INVISIBLE);
