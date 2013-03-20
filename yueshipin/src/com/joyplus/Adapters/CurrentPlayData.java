ffd4649224e3e98c5862fa1e1e9902389396ec2e
YuMinghe <veteranyu@163.com>
2013-03-20 23:33:48 +0800

    yu

---
+++ yueshipin/src/com/joyplus/Adapters/CurrentPlayData.java
@@ -0,0 +1,9 @@
+package com.joyplus.Adapters;
+
+public class CurrentPlayData {
+	public String prod_id;
+	public int CurrentCategory = 0;
+	public int CurrentIndex = 0;
+	public int CurrentSource = 0;
+	public int CurrentQuality = 0;
+}
